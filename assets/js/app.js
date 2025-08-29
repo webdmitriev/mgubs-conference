e6bd7afbe47a2de99fb754c50f88c97a22e5aeb9
webdmitriev <webdmitriev@gmail.com>
2025-08-29 15:35:27 +0300

    conference-block-08 is ready - Слайдер

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,4 +15,29 @@ document.addEventListener('DOMContentLoaded', function () {
     })
   })
 
+  // conference-block-08
+  $('.conference-slider').each(function () {
+    const slider = $(this);
+    slider.slick({
+      autoplay: true,
+      autoplaySpeed: 700000,
+      dots: false,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      variableWidth: true,
+      prevArrow: false,
+      nextArrow: $('.conference-block-08 .conference-slider__next'),
+      rows: 0,
+      responsive: [
+        {
+          breakpoint: 1199,
+          settings: {
+            // centerMode: true,
+          }
+        }
+      ]
+    });
+  })
+
 });
